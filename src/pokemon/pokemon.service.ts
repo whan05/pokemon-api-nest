9c96a4c0faceeb2af624f98e3d5277e0cf8a2c1b
whan05 <wfonsecap-m@ucenfotec.ac.cr>
2025-10-28 20:30:16 -0600

    feat: updated v3

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -10,13 +10,22 @@ import { isValidObjectId, Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
+
+  private defaultLimit: number;
+
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
-  ) {}
+    private readonly configService: ConfigService,
+  ) {
+
+    this.defaultLimit = configService.get<number>('defaultLimit') ?? 7
+  }
+
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
@@ -32,7 +41,8 @@ export class PokemonService {
 
   findAll( paginationDto: PaginationDto) {
 
-    const { limit = 10, offset = 0} = paginationDto
+    
+    const { limit = this.defaultLimit , offset = 0} = paginationDto
     return this.pokemonModel.find().limit(limit).skip(offset).sort({no: 1}).select("-__v");
   }
 
